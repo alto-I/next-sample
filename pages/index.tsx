09e61a38a64e9814160c9bbd49477d2e16571d02
ito_arata <blmaldiy@gmail.com>
2022-11-23 12:21:50 +0900

    4.2まで

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  color: red;
+`;
+
+const Badge = styled.span`
+  padding: 8px 16px;
+  font-weight: bold;
+  text-align: center;
+  color: white;
+  background: red;
+  border-radius: 16px;
+`;
 
 export default function Home() {
   return (
@@ -12,9 +26,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <H1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </H1>
+        <Badge>Hello World!</Badge>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -67,5 +82,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
